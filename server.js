24294bf4ac817ac244e6c962625cbae6798a9135
emilykidd3 <e.kidd61@yahoo.com>
2021-02-25 13:06:23 -0800

    used to join to combine party name into the candidate table

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 // get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = 'SELECT * FROM candidates';
+    const sql = 'SELECT candidates.*, parties.name AS party_name FROM candidates LEFT JOIN parties ON candidates.party_id = parties.id';
     const params = [];
     db.all(sql, params, (err, rows) => {
         if(err) {
@@ -38,7 +38,7 @@ app.get('/api/candidates', (req, res) => {
 
 // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = 'SELECT * FROM candidates WHERE ID = ?';
+    const sql = 'SELECT candidates.*, parties.name AS party_name FROM candidates LEFT JOIN parties ON candidates.party_id = parties.id WHERE candidates id = ?';
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
